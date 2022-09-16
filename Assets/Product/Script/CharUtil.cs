7a7be02e6d9424187490744803c542f06062a42c
dosusang <2501488984@qq.com>
2022-09-16 14:38:47 +0800

    刚体修改

--- Assets/Product/Script/CharUtil.cs
+++ Assets/Product/Script/CharUtil.cs
@@ -2,6 +2,17 @@
 
 public class CharUtil : MonoBehaviour
 {
+    private Rigidbody rb;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        rb.velocity = Vector3.zero;
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         GameMgr.Instance.GameMain.OnCollide(gameObject.GetInstanceID(), other.gameObject.GetInstanceID());
